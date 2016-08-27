bb4365b69d602ddc377ecb6ab02c83e17d5332cf
Aatish Sai <meatish.sai@gmail.com>
2016-08-27 16:13:37 +0545

    created a matrix for rating with user_id and movie_title

--- recommendatoin.py
+++ recommendatoin.py
@@ -7,3 +7,9 @@ ratings_df = pd.read_table('ml-1m/ratings.dat',header=None,sep='::',names=['user
 del ratings_df['timestamp']
 
 ratings_df = pd.merge(ratings_df,movies_df,on='movie_id')[['user_id','movie_title','movie_id','rating']]
+
+ratings_mtx_df = ratings_df.pivot_table(values='rating',index='user_id',columns='movie_title')
+
+ratings_mtx_df.fillna(0,inplace=True)
+
+movie_index = ratings_mtx_df.columns
