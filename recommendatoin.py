001389470fb52bf480cb8d4b11c7d471e809b8da
Aatish Sai <meatish.sai@gmail.com>
2016-08-27 16:07:30 +0545

    reading movies data file

--- recommendatoin.py
+++ recommendatoin.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+movies_df = pd.read_table('ml-1m/movies.dat',header=None,sep='::',names=['movie_id','movie_title','movie_genre'])
+
 ratings_df = pd.read_table('ml-1m/ratings.dat',header=None,sep='::',names=['user_id','movie_id','rating','timestamp'])
 
 del ratings_df['timestamp']
