7bc31c6e4771f277e5f004ec648742d380a5a8a6
Aatish Sai <meatish.sai@gmail.com>
2016-08-27 16:09:28 +0545

    merge two table to create a compact table

--- recommendatoin.py
+++ recommendatoin.py
@@ -5,3 +5,5 @@ movies_df = pd.read_table('ml-1m/movies.dat',header=None,sep='::',names=['movie_
 ratings_df = pd.read_table('ml-1m/ratings.dat',header=None,sep='::',names=['user_id','movie_id','rating','timestamp'])
 
 del ratings_df['timestamp']
+
+ratings_df = pd.merge(ratings_df,movies_df,on='movie_id')[['user_id','movie_title','movie_id','rating']]
