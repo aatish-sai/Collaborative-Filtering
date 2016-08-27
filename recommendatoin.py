884afd4ad436892a0374910070f0aa9bc9041d08
Aatish Sai <meatish.sai@gmail.com>
2016-08-27 16:27:29 +0545

    function to give movie similarity and recommendation

--- recommendatoin.py
+++ recommendatoin.py
@@ -17,3 +17,36 @@ ratings_mtx_df.fillna(0,inplace=True)
 movie_index = ratings_mtx_df.columns
 
 corr_matrix = np.corrcoef(ratings_mtx_df.T)
+
+def get_movie_similarity(movie_title):
+
+    movie_idx = list(movie_index).index(movie_title)
+
+    return corr_matrix[movie_idx]
+
+def get_movie_recommendation(user_movies):
+
+    movie_similarity = np.zeros(corr_matrix.shape[0])
+
+    for movie_id in user_movies:
+
+        movie_similarity = movie_similarity + get_movie_similarity(movie_id)
+
+    similarity_df = pd.DataFrame({
+        'movie_title' : 'movie_index',
+        'sum_similarity' : 'movie_similarity'
+        })
+
+    similarity_df = similarity_df[~(similarity_df.movie_title.isin(user_movies))]
+
+    similarity_df = similarity_df.sort_values(by=['sum_similarity'],ascending=False)
+
+    return similarity_df
+
+sample_user = 30
+
+sample_user_movies = ratings_df[ratings_df.user_id==sample_user].movie_title.tolisy()
+
+recommendation = get_movie_recommendation(sample_user_movies)
+
+recommendation.movie_title.head(20)
